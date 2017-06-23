a5c348557266ad2042e845c1d0d1d6e1538f3b87
Loïc Serafin <loicserafin@gmail.com>
2017-06-23 14:30:59 +0200

    readme lab7

---
+++ labos/lab7/evolution.py
@@ -0,0 +1,80 @@
+# Loic Serafin
+import vtk
+
+white = (1, 1, 1)
+top_left = (0, 0.5, 0.5, 1)
+top_right = (0.5, 0.5, 1, 1)
+bottom_left = (0, 0, 0.5, 0.5)
+bottom_right = (0.5, 0, 1, 0.5)
+palette = [(0, 0.225, 0.28, 0.765), (127, 0.867, 0.867, 0.867), (255, 0.714, 0, 0.127)]
+
+camera = vtk.vtkCamera()
+camera.SetViewUp(0, 0, 1)
+camera.SetPosition(0, 1, 0)
+
+def generate_volume_from_slc(file, opacity_points, viewport, rgb_points=palette, camera=camera, background_color=white):
+    reader = vtk.vtkSLCReader()
+    reader.SetFileName(file)
+
+    opacity = vtk.vtkPiecewiseFunction()
+    for op_point in opacity_points:
+        opacity.AddPoint(*op_point)
+
+    color = vtk.vtkColorTransferFunction()
+    for color_point in rgb_points:
+        color.AddRGBPoint(*color_point)
+
+    volumeProperty = vtk.vtkVolumeProperty()
+    volumeProperty.SetColor(color)
+    volumeProperty.SetScalarOpacity(opacity)
+    volumeProperty.SetInterpolationTypeToLinear()
+
+    composite = vtk.vtkVolumeRayCastCompositeFunction()
+    mapper = vtk.vtkVolumeRayCastMapper()
+    mapper.SetVolumeRayCastFunction(composite)
+    mapper.SetInputConnection(reader.GetOutputPort())
+
+    volume = vtk.vtkVolume()
+    volume.SetMapper(mapper)
+    volume.SetProperty(volumeProperty)
+
+    renderer = vtk.vtkRenderer()
+    renderer.SetBackground(*background_color)
+    renderer.SetViewport(*viewport)
+    renderer.AddVolume(volume)
+    renderer.SetActiveCamera(camera)
+    renderer.ResetCamera()
+
+    return renderer
+
+
+stage10 = generate_volume_from_slc("data/ts10.slc",
+                                   [(0, 0), (72.290, 0), (175.561, 0.022), (255, 0)],
+                                   top_left)
+stage11 = generate_volume_from_slc("data/ts11.slc",
+                                   [(0, 0), (101.682, 0), (140.514, 0.03), (255, 0)],
+                                   top_right)
+stage12 = generate_volume_from_slc("data/ts12.slc",
+                                   [(0, 0), (101.682, 0), (112.4, 0.03), (255, 0)],
+                                   bottom_left)
+stage13 = generate_volume_from_slc("data/ts13.slc",
+                                   [(0, 0), (101.682, 0), (125.65, 0.1), (255, 0)],
+                                   bottom_right)
+
+
+
+ren_win = vtk.vtkRenderWindow()
+ren_win.AddRenderer(stage10)
+ren_win.AddRenderer(stage11)
+ren_win.AddRenderer(stage12)
+ren_win.AddRenderer(stage13)
+
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(ren_win)
+ren_win.SetSize(1200, 800)
+style = vtk.vtkInteractorStyleTrackballCamera()
+iren.SetInteractorStyle(style)
+
+ren_win.Render()
+
+iren.Start()
---
+++ labos/lab7/readme.txt
@@ -0,0 +1,23 @@
+VTK - Visualisation du développement de l'embryon de la souris
+==============================================================
+
+Ce laboratoire est en deux parties:
+- Une visualisation de l'évolution de l'embryon d'une souris en 4 étapes (ts10, ts11, ts12 et ts13)
+- Une visualisation à l'étape ts14 des différents organes
+
+Pour la première partie (fichier evolution.py), les embryons sont affichés comme des volumes, en appliquant une fonction
+par morceaux d'opacité. Cette fonction d'opacité a été obtenue en regardant les résultats directement avec le programme
+Paraview.
+
+
+Pour la seconde partie (fichier organs.py), la même méthode a été utilisée, mais la palette de couleur a été adaptée
+pour afficher des couleurs différentes dans les différents segments scalaires correspondants aux différentes régions
+intéressantes. L'opacité des régions intermédiaires à ces régions est à 0, ce qui nous permet de bien voir les régions
+internes. Les régions n'étant pas définies très clairement, et incluant des zones très différentes (ex: le scalaire 226
+qui définit la vésicule optique définit également des points sur le derme de l'embryon), une opacité très faible a été
+adaptée pour chacune de ces régions, pour ne rendre visible que le noyau central de l'organe.
+
+À noter que les deux carotides ne sont pas bien définis, et s'attribuent à la même zone que pour la vésicule optique.
+
+
+Dans les deux parties, les volumes sont rendus avec du RayCasting.
